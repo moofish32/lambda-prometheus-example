83fb60dcb6e2e07361366b51a4868f51626eae34
Mike Cowgill <mcowgill@stripe.com>
2023-02-24 15:44:24 -0800

    initial layer work

---
+++ lambda/handler.py
@@ -0,0 +1,33 @@
+from opentelemetry import metrics
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+
+# Service name is required for most backends
+resource = Resource(attributes={
+    SERVICE_NAME: "reproduction-issue"
+})
+
+reader = PeriodicExportingMetricReader(
+    OTLPMetricExporter()
+)
+provider = MeterProvider(resource=resource, metric_readers=[reader])
+metrics.set_meter_provider(provider)
+meter = metrics.get_meter(__name__)
+
+cold_start_counter = meter.create_counter(
+    "cold_start_counter",
+    description="The number of cold_starts",
+)
+test_counter = meter.create_counter(
+    "test_counter",
+    description="The number of counts",
+)
+
+def handler(event, context):
+    print(context)
+    print('writing metric')
+    test_counter.add(1, {'env': 'dev'})
+    print('done')
+    return
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,86 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { App, Duration, Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import { aws_aps as aps } from 'aws-cdk-lib'
+import { aws_grafana as grafana } from 'aws-cdk-lib';
+import { aws_iam as iam } from 'aws-cdk-lib'
+import { aws_lambda as lambda } from 'aws-cdk-lib'
+import { aws_logs as logs } from 'aws-cdk-lib'
+import * as lambdaPython from '@aws-cdk/aws-lambda-python-alpha'
+import * as path from 'path'
 
-export class MyStack extends Stack {
-  constructor(scope: Construct, id: string, props: StackProps = {}) {
-    super(scope, id, props);
+export class ReproObservabilityStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps) {
+    super(scope, id, props)
 
-    // define resources here...
+    const namingPrefix = 'prometheus-repro'
+
+    const logGroup = new logs.LogGroup(this, 'ReproObservabilityLogGroup', {
+      logGroupName: `/aws/prometheus/${namingPrefix}-dev`,
+      removalPolicy: RemovalPolicy.DESTROY,
+    })
+
+    new aps.CfnWorkspace(this, 'ReproductionPrometheusWorkspace', {
+      alias: namingPrefix,
+      loggingConfiguration: {
+        logGroupArn: logGroup.logGroupArn,
+      },
+      tags: [{
+        key: 'Name',
+        value: namingPrefix,
+      }],
+    })
+    const grafanaRole = new iam.Role(this, 'ReproGrafanaRole', {
+      roleName: `prom-repro-grafana-dev`,
+      assumedBy: new iam.ServicePrincipal('grafana.amazonaws.com'),
+      managedPolicies: [{
+        managedPolicyArn: 'arn:aws:iam::aws:policy/AmazonPrometheusFullAccess'
+      }],
+    })
+    new grafana.CfnWorkspace(this, 'ReproGrafanaWorkspace', {
+      accountAccessType: 'CURRENT_ACCOUNT',
+      authenticationProviders: ['AWS_SSO'],
+      name: 'ReproGrafana',
+      description: 'Observability example',
+      permissionType: 'SERVICE_MANAGED',
+      roleArn: grafanaRole.roleArn,
+      dataSources: ['PROMETHEUS'],
+    });
+
+    const otelLayerArn = "arn:aws:lambda:us-east-2:901920570463:layer:aws-otel-python-amd64-ver-1-15-0:1"
+    const otelLayer = lambda.LayerVersion.fromLayerVersionArn(this, 'OtelLayer', otelLayerArn)
+
+    const reproLambda = new lambdaPython.PythonFunction(this, 'ReproPrometheusLambda', {
+      functionName: `${namingPrefix}-dev`,
+      description: 'Validate lambda can write to APS',
+      entry: path.join(__dirname, '..', 'lambda'),
+      runtime: lambda.Runtime.PYTHON_3_9,
+      index: 'handler.py',
+      handler: 'handler',
+      timeout: Duration.minutes(1),
+      memorySize: 256,
+      retryAttempts: 0,
+      reservedConcurrentExecutions: 150,
+      tracing: lambda.Tracing.ACTIVE,
+      environment: {
+        ENVIRONMENT: 'dev',
+        OPENTELEMETRY_COLLECTOR_CONFIG_FILE: '/var/task/collector.yaml',
+        AWS_LAMBDA_EXEC_WRAPPER: '/opt/otel-instrument',
+        OTEL_METRICS_EXPORTER: 'otlp_proto_http',
+        NAMING_PREFIX: namingPrefix,
+      },
+      layers: [otelLayer],
+    })
+    reproLambda.role?.addToPrincipalPolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ['*'],
+      actions: [
+        'aps:RemoteWrite',
+        'aps:GetSeries',
+        'aps:GetLabels',
+        'aps:GetMetricMetadata',
+      ]
+    }))
+
   }
 }
 
@@ -17,7 +92,7 @@ const devEnv = {
 
 const app = new App();
 
-new MyStack(app, 'proserve-example-dev', { env: devEnv });
+new ReproObservabilityStack(app, 'proserve-example-dev', { env: devEnv });
 // new MyStack(app, 'proserve-example-prod', { env: prodEnv });
 
 app.synth();
